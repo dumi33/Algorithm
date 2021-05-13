e052e4c5eb695f598320a69e9873286390302fad
dumi33 <75162315+dumi33@users.noreply.github.com>
2021-05-13 21:16:44 +0900

    Update BFS_단지번호붙이기.cpp

--- BFS/BFS_단지번호붙이기.cpp
+++ BFS/BFS_단지번호붙이기.cpp
@@ -1,49 +1,55 @@
-#include<iostream>
-#include<queue>
-#define x first
-#define y second
-int dx[4] = { 0,1,0,-1 };
-int dy[4] = { 1,0,-1,0 };
-int board[26][26];
-int dis[26][26];
+#include <iostream>
+#include <queue>
+#include <algorithm>
+#include <vector>
 using namespace std;
+
+int dx[4] = { 1,0,-1,0 };
+int dy[4] = { 0,1,0,-1 };
+
+#define X first
+#define Y second
+
+string board[26];
+bool visit[26][26];
+
 int main() {
-    queue<pair<int, int> > Q;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) cin >> board[i];
+	queue<pair<int, int>> Q;
+	int num = 0;
+	vector<int> v;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (board[i][j] == '0' || visit[i][j]) continue;
+			num++;
+			Q.push({ i, j });
+			visit[i][j] = true;
+			int size = 0;
+			while (!Q.empty()) {
+				size++;
+				pair<int, int> cur = Q.front();
+				Q.pop();
+				for (int i = 0; i < 4; i++) {
+					int nx = cur.X + dx[i];
+					int ny = cur.Y + dy[i];
+					if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+					if (board[nx][ny] == '0' || visit[nx][ny]) continue;
+					Q.push({ nx, ny });
+					visit[nx][ny] = true;
+				}
 
-    int N, count;
-    cin >> N; //N*N의 배열
-    for (int i = 0; i < N; i++) {
-        fill(dis[i], dis[i] + N, -1); //-1로 초기화
-    }
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            cin >> board[i][j]; //입력받음
-        }
-    }
+			}
+			v.push_back(size);
+		}
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            int a;
-            if (dis[i][j] != -1 || board[i][j] != 1) continue;
-            Q.push({ i,j });
-            while (!Q.empty()) {
-                pair<int, int> cur = Q.front();
-                Q.pop();
-                count++;
-                for (int dir = 0; dir < 4; dir++) {
-                    int nx = cur.x + dx[dir];
-                    int ny = cur.y + dy[dir];
-                    if (dis[i][j] != -1 || board[i][j] != 1) continue;
-                    a++;
-                    Q.push({ i,j });
-                    dis[i][j] = dis[cur.x][cur.y] + 1;
-                }
-            }
-            cout << a << "\n";
-
-        }
-    }
-
+	}
+	sort(v.begin(), v.end());
+	cout << num << '\n';
+	for (int i : v) cout << i << '\n';
 }
 
 
