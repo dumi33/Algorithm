a318f725350ffb5e0faf92fb8f258ec4458fc236
dumi33 <75162315+dumi33@users.noreply.github.com>
2021-07-09 11:14:47 +0900

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     TextView textview;
     EditText editText;
     Button button;
+    Switch sw;
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,20 +26,29 @@ public class MainActivity extends AppCompatActivity {
         textview = findViewById(R.id.textView);
         editText = findViewById(R.id.editTextNumberSigned);
         button = findViewById(R.id.button);
+        sw = findViewById(R.id.switch1);
+        image = findViewById(R.id.imageView);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 //문자열을 입력
-                String strC = editText.getText().toString();
-                if(strC.equals(""))
-                    return;
-                //처리 : c -> F
-                double c = Double.parseDouble(strC); //실수로 변환
-                double f = 9/5.0 * c + 32;
-                //textview 출력
-
-                textview.setText(""+f);
+                if(!sw.isChecked()) {
+                    String strC = editText.getText().toString();
+                    if (strC.equals(""))
+                        return;
+                    //처리 :  c -> F
+                    double c = Double.parseDouble(strC); //실수로 변환
+                    double f = 9 / 5.0 * c + 32;
+                    //textview 출력
+                    textview.setText("switch" + f);
+                    image.setImageResource(R.drawable.ic_launcher_foreground);
+                }
+                else {
+                    //f->C
+                    textview.setText("switch true");
+                    image.setImageResource(R.drawable.sujin);
+                }
             }
         });
     }
