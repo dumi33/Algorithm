fd8a23772ed4008399d3a76636d0e964bf6d504e
dumi33 <75162315+dumi33@users.noreply.github.com>
2022-07-11 22:07:27 +0900

    Create DFS_치킨배달.py

---
+++ 알고리즘/삼성역량테스트/DFS_치킨배달.py
@@ -0,0 +1,37 @@
+n,m = map(int,input().split())
+mp =  [list(map(int,input().split())) for i in range (n)]
+
+hs = []
+ch = []
+cb = [0]*m # 조합 
+res = int(1e9)
+
+
+def dfs(L,s) :
+    global res
+    if L==m: # 치킨집의 개수가 m 개 
+        sum = 0 # 도시의 피자거리
+        for j in range(len(hs)) : # 집들을 돌면서 
+            x1 = hs[j][0]
+            y1 = hs[j][1]
+            dis = int(1e9)
+            for x in cb : #   치킨집 좌표 
+                x2 = ch[x][0]
+                y2 = ch[x][1]
+                dis = min(dis, abs(x2-x1)+abs(y1-y2))
+            sum += dis 
+        if sum <res : res = sum
+        
+    else :
+        for i in range(s,len(ch)) :
+            cb[L] = i
+            dfs(L+1,i+1) 
+            
+for i in range(n) :
+    for j in range(n) :
+        if mp[i][j] == 1 : #  집 발견 
+            hs.append((i,j))
+        elif mp[i][j]==2 : # 치킨집 발결 
+            ch.append((i,j))
+dfs(0,0)
+print(res)
