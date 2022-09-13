8adb9b01f3cea6dfd2c49894e1ac17028e49672c
dumi33 <75162315+dumi33@users.noreply.github.com>
2022-09-13 11:00:44 +0900

    Update 가장긴증가하는부분수열.py

--- 알고리즘/dp/가장긴증가하는부분수열.py
+++ 알고리즘/dp/가장긴증가하는부분수열.py
@@ -1,10 +1,16 @@
 if __name__=="__main__" :
     n = int(input())
     arr = list(map(int,input().split()))
+    
+    #  적어도 길이가 1
     dy = [1]*n
+    
     for i in range(1,n) :
+        # 앞의 원소들을 보며 
         for j in range(i) :
+            # 작다면 
             if arr[i] > arr[j] :
+                # 갱신
                 dy[i] = max(dy[i],dy[j]+1)
     print(max(dy)) 
             
