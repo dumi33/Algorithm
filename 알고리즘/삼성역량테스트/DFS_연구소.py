552faa970ad8f6d8f2b9537b96e08bb671c743c1
dumi33 <75162315+dumi33@users.noreply.github.com>
2022-09-14 13:40:01 +0900

    Update DFS_연구소.py

--- 알고리즘/삼성역량테스트/DFS_연구소.py
+++ 알고리즘/삼성역량테스트/DFS_연구소.py
@@ -1,47 +1,49 @@
-n,m =map(int,input().split())
-mp = [list(map(int,input().split())) for i in range(n)]
-vis = [[0]*m for i in range(n)]
-ans = 0 # 최대 안전영역 
-dx = [-1,0,1,0]
-dy = [0,1,0,-1]
-tmp = [[0]*m for i in range(n)]
+from collections import deque
+import copy
+
+           
+# 동 서 북 남      
+dx = [0,0,-1,1]
+dy = [1,-1,0,0]
 
 
-def virus(x,y) : # (x,y)로부터 바이러스를 퍼트린다. 
-    for i in range(4) :
-        nx = dx[i] + x
-        ny = dy[i] + y
-        if 0<=nx<n and 0<=ny<m and tmp[nx][ny] == 0 :
-            tmp[nx][ny] = 2
-            virus(nx,ny)
-
-def Score() : # 안전영역의 개수를 구한다. 
-    score = 0
+def bfs() :
+    global ans 
+    tmp_mp = copy.deepcopy(mp)    
     for i in range(n) :
         for j in range(m) :
-            if tmp[i][j] == 0:
-                score +=1
-    return score
+            if tmp_mp[i][j]==2 :
+                q.append([i,j])
+    while q :
+        x,y = q.popleft()
+        for dir in range(4) :
+            nx,ny = x + dx[dir], y + dy[dir]
+            if 0<=nx<n and 0<=ny<m and tmp_mp[nx][ny] == 0 :
+                tmp_mp[nx][ny] = 2 
+                q.append([nx,ny]
+    cnt = 0
+    for i in tmp_mp :
+        cnt += i.count(0)
+    ans = max(ans,cnt)
+                         
 
-def dfs(count) :
-    global ans
-    if count == 3 : # 벽의 개수가 3개라면 
-        for i in range(n) :
-            for j in range(m) :
-                tmp[i][j] = mp[i][j] # tmp로 복사
-        for i in range(n) :
-            for j in range(m) :
-                if tmp[i][j] == 2 : # 바이러스가 있다면
-                    virus(i,j) # 퍼트려
-        ans = max(ans,Score()) #점수를 갱신 
-        return
-    for i in range(n) : 
-        for j in range(m) :
-            if count< 3 and mp[i][j] == 0 :
-                mp[i][j] = 1 # 벽을 세워
-                count+=1 
-                dfs(count)
-                mp[i][j] = 0
-                count-=1 
-dfs(0)
-print(ans)
+def wall(x) :
+    if x == 3 :
+        bfs()
+        return 
+    for i in range(n) :
+        for j in  range(m) :
+            if mp[i][j] == 0:
+                mp[i][j] = 1
+                wall(x+1)
+                mp[i][j] = 0 
+
+
+if __name__=="__main__" :
+    n,m = map(int,input().split())
+    mp = [list(map(int,input().split())) for _ in range(n)]
+    ans = 0
+    q = deque()
+
+    wall(0)
+    print(ans)
